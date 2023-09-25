49cca53815952cdd4137487630ddbb678de406d5
Rohit-iOS-Dev <rohit.xitijinfo@gmail.com>
2023-09-25 12:11:55 +0530

    final

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings: ['iOS Developer', 'Mobile App developer', 'Team leader'],
+    strings: ['iOS Developer', 'Mobile App developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
